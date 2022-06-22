c5f9dda775befd41b9671d4469e00ae2db3f252a
Michael John Santos <michaeljohnsantos@student.mitt.ca>
2022-06-22 13:52:50 -0500

    Remove unnecessary comments

--- SD-300-Lab-1/Program.cs
+++ SD-300-Lab-1/Program.cs
@@ -24,166 +24,6 @@ money.Add(1);
 
 LobbyVM.VendItem("asd", money);
 
-/* This function represents the whole operation of a Purchase in the vending machine, it accepts a vending machine dictionary,
- the price of an item and the money that's been cashed in */
-//Dictionary<int, int> Purchase(Dictionary<int, int> vend
-//{
-//    Dictionary<int, int> changeDenomination = new Dictionary<int, int>();
-//    int change = 0;
-//    if (cashIn >= price)
-//    {
-//        change = cashIn - price;
-//        int subtractor = change;
-//        subtractor = cashIn - price;
-//        int number = 0;
-
-//        for (int i = vendingMachine.Count - 1; i >= 0; i--)
-//        {
-//            int floatCoins = vendingMachine.ElementAt(i).Value;
-//            if (vendingMachine.ElementAt(i).Key <= subtractor && floatCoins > 0)
-//            {
-//                int pieces = 0;
-
-//                if (cashIn / vendingMachine.ElementAt(i).Key >= 2)
-//                {
-//                    number = subtractor / vendingMachine.ElementAt(i).Key;
-//                    pieces += number;
-//                }
-
-//                subtractor -= vendingMachine.ElementAt(i).Key * pieces;
-//                changeDenomination.Add(vendingMachine.ElementAt(i).Key, pieces);
-//                floatCoins -= pieces;
-//            }
-//        }
-
-//    }
-//    else
-//    {
-//        Console.WriteLine("Cash entered is not enough");
-//    }
-
-//    Console.WriteLine($"You have entered {cashIn} for an item that costs {price}");
-//    Console.WriteLine($"You'll have a total change of {change}:");
-//    foreach (KeyValuePair<int, int> coin in changeDenomination)
-//    {
-//        Console.WriteLine($"{coin.Value} pc(s) of ${coin.Key}");
-//    }
-
-//    return changeDenomination;
-//}ingMachine, int price, int cashIn)
-//class Department
-//{
-//    public string Name { get; set; }
-//    public ICollection<Student> Students { get; set; }
-//    public ICollection<Course> Courses { get; set; }
-//    public ICollection<Enrollment> Enrollments { get; set; }
-//    public int CourseNumberCount { get; set; } = 1;
-//    public int EnrollmentNumberCount { get; set; } = 1;
-//    public int StudentId { get; set; } = 1;
-//    public Department(string name)
-//    {
-//        Name = name;
-//        Students = new HashSet<Student>();
-//        Courses = new HashSet<Course>();
-//        Enrollments = new HashSet<Enrollment>();
-//    }
-
-//    public Course CreateCourse(string courseTitle)
-//    {
-//        Course course = new Course(courseTitle, CourseNumberCount);
-//        CourseNumberCount++;
-
-//        Courses.Add(course);
-//        return course;
-//    }
-
-//    public Student CreateStudent(string studentName)
-//    {
-
-//        Student newStudent = new Student(studentName, StudentId);
-//        StudentId++;
-
-//        Students.Add(newStudent);
-//        return newStudent;
-//    }
-
-//    public Student RegisterStudent(string name)
-//    {
-//        Student student = new Student(name, StudentId);
-//        Students.Add(student);
-//        student.Department = this;
-
-//        return student;
-//    }
-
-//    public void DeleteEnrollment(int enrollmentId)
-//    {
-//        try
-//        {
-//            //Enrollment enrollment = GetEnrollment(enrollmentId);
-//            foreach (Enrollment e in Enrollments)
-//            {
-//                if (e.RegistrationId.Equals(enrollmentId))
-//                {
-//                    Enrollments.Remove(e);
-//                }
-//            }
-//        }
-//        catch (Exception ex)
-//        {
-//            Console.WriteLine(ex.Message);
-//        }
-//    }
-
-
-//    public Student GetStudent(int id)
-//    {
-//        Student student;
-//        foreach (Student s in Students)
-//        {
-//            if (s.StudentId == id)
-//            {
-//                student = s;
-//                return student;
-//            }
-//        }
-//        throw new Exception("Student not found");
-//    }
-
-//    public Course GetCourseId(int id)
-//    {
-//        Course course;
-//        foreach (Course c in Courses)
-//        {
-//            if (c.CourseNumber.Equals(id))
-//            {
-//                course = c;
-//                return course;
-//            }
-//        }
-//        throw new Exception("Course not found");
-//    }
-
-//    public void EnrollStudent(int studentId, int courseId)
-//    {
-//        try
-//        {
-//            Student student = GetStudent(studentId);
-//            Course course = GetCourseId(courseId);
-
-//            Enrollment newEnrollment = new Enrollment(student, course);
-//            Enrollments.Add(newEnrollment);
-
-//            student.Courses.Add(newEnrollment);
-//            course.Enrollments.Add(newEnrollment);
-//        }
-//        catch (Exception ex)
-//        {
-//            Console.WriteLine(ex.Message);
-//        }
-//    }
-//}
-
 class VendingMachine
 {
     public int SerialNumber { get; set; } = 1;
@@ -241,10 +81,33 @@ class VendingMachine
         }
         return $"Great job!";
     }
-    //public void ComputeTotalCashIn(List<int> insertedMoney)
-    //{
-    //    for(insertedMoneyCount)
-    //}
+
+    public Dictionary<int, int> GetDenominationOfChange(int totalChange, Dictionary<int, int> moneyFloat)
+    {
+        Dictionary<int, int> change = new Dictionary<int, int>();
+        // list the money denomination change
+        for(int i = moneyFloat.Count - 1; i >= 0; i--)
+        {
+            
+            int integer = moneyFloat.ElementAt(i).Key;
+            int pieces = 0;
+            int subtractor = 0;
+            if(totalChange / integer >= 2)
+            {
+                pieces = totalChange / integer;
+                subtractor -= pieces * integer;
+                Console.WriteLine($"======================{integer}---{pieces}");
+                change.Add(integer, pieces);
+            }
+            else
+            {
+                pieces = totalChange - subtractor;
+                change.Add(integer, pieces);
+            }
+        }   
+        return change;
+    }
+
     public string VendItem(string code, List<int> insertedMoney)
     {
         int cashIn = insertedMoney[0];
@@ -261,7 +124,7 @@ class VendingMachine
             else {
                 cashIn +=  insertedMoney[i + 1];
             }
-            Console.WriteLine($"Successfully processed {insertedMoney[i]} total of ${cashIn}");
+            Console.WriteLine($"Successfully inserted {insertedMoney[i]}");
 
             foreach (KeyValuePair<int, int> money in MoneyFloat)
             {
@@ -269,10 +132,10 @@ class VendingMachine
                 if (insertedMoney[i] == money.Key)
                 {
                     MoneyFloat[money.Key] += 1;
-                    Console.WriteLine($"You have inserted {money.Key}");
                 }
             }
         }
+        Console.WriteLine($"You've inserted a total of ${cashIn}");
         int totalChange = 0;
         foreach (KeyValuePair<Product, int> pair in Inventory)
         {
@@ -280,52 +143,42 @@ class VendingMachine
             // if the product exist in the inventory or in-stock
             if (pair.Key.Code == code)
             {
+                Console.WriteLine("================================================");
+                Console.WriteLine($"You have selected {pair.Key.Name}");
+                Console.WriteLine($"Price is ${pair.Key.Price} each");
 
-                Console.WriteLine($"This is {pair.Key.Code}");
-                Console.WriteLine($"Price is {pair.Key.Price}");
                 // updates the inventory
+                Console.WriteLine($"00000000000000000000 {Inventory[pair.Key] - 1}");
                 Inventory[pair.Key] -= 1;
-                List<int> change = new List<int>();
                 
+                List<int> change = new List<int>();
+                Console.WriteLine($"There are {pair.Value} pc(s) remaining for ");
                 foreach (KeyValuePair<int, int> moneyPair in MoneyFloat)
                 {
-                    //Console.WriteLine($"This is the moneypair loop");
-
                     // update the money float, vend change
                     if (cashIn > pair.Key.Price)
                     {
-                        Console.WriteLine($"So the cashIn is {cashIn} and key price is {pair.Key.Price}");
                         totalChange = cashIn - pair.Key.Price;
-                        Console.WriteLine($"This is changeTotal {totalChange}");
-                        // list the money denomincation change
-                        if (totalChange / moneyPair.Key >= 0)
-                        {
-                            //Console.WriteLine($"This is the moneypair KEY {moneyPair.Key}");
-                            change.Add(moneyPair.Key);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Please insert enough amount");
-                        }
 
-                        
                         for(int i = 0; i < change.Count; i++)
                         {
                             if (moneyPair.Key == change[i])
                             {
                                 MoneyFloat[moneyPair.Key] -= 1;
                                 Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-                                
+                                Console.WriteLine($"Successful Vend: {moneyPair.Key} has remaining {moneyPair.Value} pc(s)");
                             }
                         }
-                        
                     }
                     else if(cashIn == pair.Key.Price)
                     {
                         Console.WriteLine($"Thank you for entering the exact amount");
                     }
+
                 }
-                Console.WriteLine($"You have received {totalChange} for your change");
+                
+                
+                Console.WriteLine($"You have received a total of ${totalChange} for your change");
             }
         }
         return "Vend complete";
